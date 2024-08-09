a3d859115c59b60ad77b4f71a14f2d2b02cb5bbe
PhantomAnubis <j.gallardogomez2@gmail.com>
2024-08-09 02:22:16 +0000

    Mundo guardado 2024-08-09

---
+++ keep_alive.py
@@ -0,0 +1,7 @@
+import time
+import sys
+
+while True:
+    print("Manteniendo el Codespace activo")
+    sys.stdout.flush()  # Fuerza que el texto se escriba en nohup.out inmediatamente
+    time.sleep(30)
